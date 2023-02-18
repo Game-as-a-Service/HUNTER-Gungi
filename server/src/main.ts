45987fbb002b88a952e33333a600a5ed497e9286
Allen Hsiao <weiping@hsiao.ca>
2023-02-18 10:19:54 -0500

    removed cats service and set port as a variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 8000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create([AppModule]);
+  await app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 }
 bootstrap();
