c5a2bc6b74aeffc9ae5ca64f503333060f45bcd8
Allen Hsiao <weiping@hsiao.ca>
2023-02-18 10:57:33 -0500

    fixed build error

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const PORT = 8000;
 
 async function bootstrap() {
-  const app = await NestFactory.create([AppModule]);
+  const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
