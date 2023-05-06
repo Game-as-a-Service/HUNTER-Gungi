2912a8e810965870b4475f6caa061f6fc3f405c9
zhih <fufong79570@gmail.com>
2023-05-06 23:31:52 +0800

    Implent DeadArea.toData

--- server/src/domain/DeadArea.ts
+++ server/src/domain/DeadArea.ts
@@ -1,3 +1,14 @@
-class DeadArea {}
+import Goma from './goma/Goma';
+import { DeadAreaData } from '../frameworks/data-services/gungi-data';
+
+class DeadArea {
+  constructor(private gomas: Goma[]) {}
+
+  toData(): DeadAreaData {
+    return {
+      gomas: this.gomas.map((goma) => goma.toData()),
+    };
+  }
+}
 
 export default DeadArea;
