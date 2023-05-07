635583126af449fd5622da7889f1525a322672c3
zhih <fufong79570@gmail.com>
2023-05-07 15:39:36 +0800

    Fix {post} surrender e2e test

--- server/test/e2e/e2e.e2e-spec.ts
+++ server/test/e2e/e2e.e2e-spec.ts
@@ -1,55 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import GungiHan from '../../src/domain/GungiHan';
-import SIDE from '../../src/domain/constant/SIDE';
 import LEVEL from '../../src/domain/constant/LEVEL';
-import player from '../../src/domain/Player';
-import GOMA from '../../src/domain/constant/GOMA';
-import GungiRepository from '../../src/data-services/GungiRepository';
-import Coord from '../../src/domain/Coord';
-import Gungi from '../../src/domain/Gungi';
 import { AppModule } from '../../src/app.module';
+import { DataServicesModule } from '../../src/frameworks/data-services/data-service-module';
+import { GungiRepository } from '../../src/frameworks/data-services/gungi-repository';
 import GungiDataModel from '../../src/frameworks/data-services/data-model/gungi-data-model';
-import GungiDao from '../../src/frameworks/data-services/dao/gungi.dao';
+import { GungiData } from '../../src/frameworks/data-services/gungi-data';
+import { randomUUID } from 'crypto';
+import SIDE from '../../src/domain/constant/SIDE';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let gungiRepository: GungiRepository;
+  let gungiDataModel: GungiDataModel;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-      providers: [],
+      imports: [AppModule, DataServicesModule],
+      providers: [GungiDataModel],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    gungiDataModel = moduleFixture.get<GungiDataModel>(GungiDataModel);
+    gungiRepository = moduleFixture.get('GungiRepository');
     await app.init();
   });
 
   it('/(POST) gungi/:gungiId/surrender', async () => {
-    const gungiDao = new GungiDao();
-    const gungiDataModel = new GungiDataModel();
-    const gungiRepository = new GungiRepository(gungiDao, gungiDataModel);
-    const gungiId = 1;
-    const players = [new player('A'), new player('B')];
-    const level = LEVEL.BEGINNER;
-    const gungiHan = new GungiHan();
-    const gungi = new Gungi(level, players, gungiHan);
-    gungi.sente = players[0];
-    gungi.gote = players[1];
+    const gungiId = randomUUID();
+    const gungiData: GungiData = {
+      currentTurn: SIDE.WHITE,
+      gungiHan: { han: [] },
+      history: [],
+      _id: gungiId,
+      level: LEVEL.BEGINNER,
+      players: [
+        {
+          id: 'A',
+          name: 'A',
+          side: SIDE.WHITE,
+          gomaOki: { gomas: [] },
+          deadArea: { gomas: [] },
+        },
+        {
+          id: 'B',
+          name: 'B',
+          side: SIDE.BLACK,
+          gomaOki: { gomas: [] },
+          deadArea: { gomas: [] },
+        },
+      ],
+    };
+
+    const gungi = gungiDataModel.toDomain(gungiData);
+
     await gungiRepository.save(gungi);
-    //要想一下這部分 因為不知道要動哪一個棋
-    gungi.ugokiGoma(
-      SIDE.BLACK,
-      GOMA.HEI,
-      new Coord(0, 1, 0),
-      new Coord(0, 0, 0),
-    );
 
     // post a  json
     const body = {
-      player: 'A',
+      playerId: 'A',
     };
+
     return request(app.getHttpServer())
       .post(`/gungi/${gungiId}/surrender`)
       .send(body)
