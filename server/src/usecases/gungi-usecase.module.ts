408a856853d6db57aa6637ac214cad38385784fb
zhih <fufong79570@gmail.com>
2023-05-07 22:13:46 +0800

    Modify token name of EventBus

--- server/src/usecases/gungi-usecase.module.ts
+++ server/src/usecases/gungi-usecase.module.ts
@@ -3,10 +3,18 @@ import SurrenderUsecase from './surrender-usecase';
 import { GungiRepositoryModule } from '../frameworks/data-services/gungiRepository.module';
 import EventBus from './eventBus';
 import FurigomaUsecase from './furigoma-usecase';
+import ImplEventBus from '../frameworks/eventBus/ImplEventBus';
 
 @Module({
   imports: [GungiRepositoryModule],
-  providers: [SurrenderUsecase, FurigomaUsecase, EventBus],
+  providers: [
+    SurrenderUsecase,
+    FurigomaUsecase,
+    {
+      provide: 'EventBus',
+      useClass: ImplEventBus,
+    },
+  ],
   exports: [SurrenderUsecase, FurigomaUsecase],
 })
 export default class GungiUsecaseModule {}
