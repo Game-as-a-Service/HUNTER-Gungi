36bb979a911c34a30fbd15a411443b4ad084101f
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-06-17 21:37:03 +0800

    extract constant

--- server/src/domain/Gungi.ts
+++ server/src/domain/Gungi.ts
@@ -11,6 +11,11 @@ import { GungiData } from '../frameworks/data-services/GungiData';
 import Goma from './goma/Goma';
 import GomaFactory from './goma/GomaFactory';
 import { ConfigurationEvent } from './events/ConfigurationEvent';
+import {
+  WHITE_HAN_CONFIG,
+  OKI_CONFIG,
+  BLACK_HAN_CONFIG,
+} from './constant/constants';
 
 class Gungi {
   constructor(
@@ -137,48 +142,6 @@ class Gungi {
         gomaOki.gomas.push(goma);
       });
     };
-
-    const WHITE_HAN_CONFIG = [
-      { name: GOMA.OSHO, x: 4, y: 0, z: 0 },
-      { name: GOMA.HEI, x: 0, y: 2, z: 0 },
-      { name: GOMA.HEI, x: 4, y: 2, z: 0 },
-      { name: GOMA.HEI, x: 8, y: 2, z: 0 },
-      { name: GOMA.SHINOBI, x: 1, y: 1, z: 0 },
-      { name: GOMA.SHINOBI, x: 7, y: 1, z: 0 },
-      { name: GOMA.YARI, x: 4, y: 1, z: 0 },
-      { name: GOMA.CHU, x: 5, y: 0, z: 0 },
-      { name: GOMA.DAI, x: 3, y: 0, z: 0 },
-      { name: GOMA.SHI, x: 3, y: 2, z: 0 },
-      { name: GOMA.SHI, x: 5, y: 2, z: 0 },
-      { name: GOMA.TORIDE, x: 2, y: 2, z: 0 },
-      { name: GOMA.TORIDE, x: 6, y: 2, z: 0 },
-    ];
-
-    const BLACK_HAN_CONFIG = [
-      { name: GOMA.OSHO, x: 4, y: 8, z: 0 },
-      { name: GOMA.HEI, x: 0, y: 6, z: 0 },
-      { name: GOMA.HEI, x: 4, y: 6, z: 0 },
-      { name: GOMA.HEI, x: 8, y: 6, z: 0 },
-      { name: GOMA.SHINOBI, x: 1, y: 7, z: 0 },
-      { name: GOMA.SHINOBI, x: 7, y: 7, z: 0 },
-      { name: GOMA.YARI, x: 4, y: 7, z: 0 },
-      { name: GOMA.CHU, x: 3, y: 8, z: 0 },
-      { name: GOMA.DAI, x: 5, y: 8, z: 0 },
-      { name: GOMA.SHI, x: 3, y: 6, z: 0 },
-      { name: GOMA.SHI, x: 5, y: 6, z: 0 },
-      { name: GOMA.TORIDE, x: 2, y: 6, z: 0 },
-      { name: GOMA.TORIDE, x: 6, y: 6, z: 0 },
-    ];
-
-    const OKI_CONFIG = [
-      { name: GOMA.HEI },
-      { name: GOMA.SHO },
-      { name: GOMA.SHO },
-      { name: GOMA.UMA },
-      { name: GOMA.UMA },
-      { name: GOMA.YARI },
-      { name: GOMA.YARI },
-    ];
 
     addGomaToHan(SIDE.WHITE, WHITE_HAN_CONFIG);
     addGomaToOki(SIDE.WHITE, OKI_CONFIG);
