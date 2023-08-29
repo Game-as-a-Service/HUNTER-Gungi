40dffde93b0a393298e358864c4cdd9e074abbf8
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2023-08-29 18:49:00 +0800

    Feature/vuejs (#22)
    * feat: delete frontend next.js project
    
    * feat: add frontend vue.js project
    
    * feat: update docker config for vue.js
    
    * ci: update ci config
    
    * docs: update README about changing next.js to vue.js
    
    * feat: add tailwind support
    
    * chore: add .nvmrc file
    
    * chore: add Dockerfile.local file and update .gitignore config
    
    * style: Add a semicolon at the end of every statement.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  env: {
+    // tailwind.config.js - error - 'module' is not defined - no-undef
+    // https://eslint.org/docs/rules/no-undef
+    node: true,
+  },
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        cat: 'red'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
