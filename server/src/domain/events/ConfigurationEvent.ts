439b5c4af91681b5d030d1cd587e6e487493bf2c
zhih <fufong79570@gmail.com>
2024-01-16 22:17:43 +0800

    Format code by linter

--- server/src/domain/events/ConfigurationEvent.ts
+++ server/src/domain/events/ConfigurationEvent.ts
@@ -1,7 +1,7 @@
 import { Event } from './Event';
 import GungiHan from '../GungiHan';
 import GomaOki from '../GomaOki';
-import EVENT_NAME from "../constant/EVENT_NAME";
+import EVENT_NAME from '../constant/EVENT_NAME';
 
 export interface ConfigurationEvent extends Event {
   name: EVENT_NAME.CONFIGURATION;
